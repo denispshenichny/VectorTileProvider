8d616b3b1741e44a1b8922b4aa0a6d567b6a396d
pshenichny.denis <denis.pshenichny@devexpress.com>
2020-04-21 18:09:58 +0300

    mbtiles split screen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using DevExpress.Map;
+using DevExpress.Xpf.Map;
 
 namespace VectorTileProvider {
 	/// <summary>
@@ -20,6 +23,28 @@ namespace VectorTileProvider {
 	public partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
+		}
+		void Grid_SizeChanged(object sender, SizeChangedEventArgs e) {
+			UpdateClip(e.NewSize.Width);
+		}
+		void Layer_ViewportChanged(object sender, ViewportChangedEventArgs e) {
+			UpdateClip(this.leftPanel.ActualWidth);
+		}
+		void UpdateClip(double width) {
+			CoordPoint newSplit = this.map.ScreenPointToCoordPoint(new Point(width, 0));
+			double splitLongitude = newSplit.GetX();
+			this.left.Bounds = new CoordPointCollection() {
+				new GeoPoint(-90, -180),
+				new GeoPoint(90, -180),
+				new GeoPoint(90, splitLongitude),
+				new GeoPoint(-90, splitLongitude)
+			};
+			this.right.Bounds = new CoordPointCollection(){
+				new GeoPoint(-90, 180),
+				new GeoPoint(90, 180),
+				new GeoPoint(90, splitLongitude),
+				new GeoPoint(-90, splitLongitude)
+			};
 		}
 	}
 }
